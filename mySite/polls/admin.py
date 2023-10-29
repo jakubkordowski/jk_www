fae78b58bc0ef9d50224c1b19d9312dc74778e22
jakubkordowski <jdzl@wp.pl>
2023-10-29 15:36:39 +0100

    zadania 1-9

--- mySite/polls/admin.py
+++ mySite/polls/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Stanowisko, Osoba
 
 admin.site.register(Question)
+
+@admin.display(description="Stanowisko (ID)")
+def stan_id(obj):
+    return f"{obj.stanowisko.nazwa} ({obj.stanowisko.id})"
+
+@admin.register(Osoba)
+class OsobaDataAdmin(admin.ModelAdmin):
+    readonly_fields = ('data_dodania',)
+    list_display = ("imie", "nazwisko", stan_id,)
+    list_filter = ("stanowisko", "data_dodania",)
+
+@admin.register(Stanowisko)
+class StanowiskoDataAdmin(admin.ModelAdmin):
+    list_filter = ("nazwa",)
--- mySite/polls/models.py
+++ mySite/polls/models.py
@@ -19,3 +19,32 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+class Stanowisko(models.Model):
+    nazwa = models.CharField(max_length=200)
+    opis = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.nazwa
+
+    class Meta:
+        verbose_name_plural = "stanowiska"
+
+class Osoba(models.Model):
+    class plec(models.IntegerChoices):
+        KOBIETA = 1
+        MEZCZYZNA = 2
+        INNE = 3
+    imie = models.CharField(max_length=20)
+    nazwisko = models.CharField(max_length=30)
+    plec = models.IntegerField(choices=plec.choices, default=plec.INNE)
+    stanowisko = models.ForeignKey(Stanowisko, on_delete=models.CASCADE)
+    data_dodania=models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.imie + " " + self.nazwisko
+
+    class Meta:
+        ordering = ["nazwisko"]
+        verbose_name_plural = "osoby"
+
